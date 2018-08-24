27b40ee74e10ba63d0a33ba0a7a07932f5a9288d
Megan Hainline <meg.hainline@gmail.com>
2018-08-24 13:19:53 -0500

    change

--- II CG 3-1/Program.cs
+++ II CG 3-1/Program.cs
@@ -6,7 +6,46 @@ namespace II_CG_3_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a number between 1-12");
+
+            string userValue = Console.ReadLine();
+            string message = "";
+
+            if (userValue == "1")
+                message = "January";
+            else if (userValue == "2")
+                message = "February";
+            else if (userValue == "3")
+                message = "March";
+            else if (userValue == "4")
+                message = "April";
+            else if (userValue == "5")
+                message = "May";
+            else if (userValue == "6")
+                message = "June";
+            else if (userValue == "7")
+                message = "July";
+            else if (userValue == "8")
+                message = "August";
+            else if (userValue == "9")
+                message = "September";
+            else if (userValue == "10")
+                message = "October";
+            else if (userValue == "11")
+                message = "November";
+            else if (userValue == "12")
+                message = "December";
+            else
+                message = "Sorry, I don't understand.";
+
+            Console.WriteLine(message);
+                    Console.ReadLine();
+
+            
+
+
+
+
         }
     }
 }
